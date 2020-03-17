a814607a25c06c38726304a943ef2bd2a837e784
wpiasecka <62281888+wpiasecka@users.noreply.github.com>
2020-03-17 20:28:04 +0100

    Add files via upload

---
+++ main.cs
@@ -0,0 +1,15 @@
+using System;
+class MainClass {
+  public static void Main (string[] args) {
+    Console.WriteLine ("Hello World");
+  }
+}
+public static void (string)[] args) {
+  Console.WriteLine ("Poniedziałek");
+}
+Console.WriteLine ("Wtorek");
+Console.WriteLine ("Środa");
+Console.WriteLine ("Czwartek");
+Console.WriteLine ("Piątek");
+Console.WriteLine ("Sobota");
+Console.WriteLine ("Niedziela");
